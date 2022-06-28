a7eeeb815b52f97aa9b64abf29fb31e45e8ac37e
Aldi Saepurahman <aldisaepurahman@gmail.com>
2022-06-28 17:51:09 +0700

    Update Modularization Project

---
+++ MySimpleLogin (modularization-solution)/app/src/main/java/com/dicoding/mysimplelogin/HomeActivity.kt
@@ -0,0 +1,48 @@
+package com.dicoding.mysimplelogin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.dicoding.core.SessionManager
+import com.dicoding.core.UserRepository
+import com.dicoding.mysimplelogin.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityHomeBinding
+    lateinit var userRepository: UserRepository
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val sesi = SessionManager(this)
+        userRepository = UserRepository.getInstance(sesi)
+
+        binding.tvWelcome.text = "Welcome ${userRepository.getUser()}"
+
+        binding.btnLogout.setOnClickListener {
+            userRepository.logoutUser()
+            moveToMainActivity()
+        }
+
+        binding.fab.setOnClickListener {
+            try {
+                moveToChatActivity()
+            } catch (e: Exception) {
+                Toast.makeText(this, "Module not found", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun moveToChatActivity() {
+        startActivity(Intent(this, Class.forName("com.dicoding.mysimplelogin.chat.ChatActivity")))
+    }
+
+    private fun moveToMainActivity() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
